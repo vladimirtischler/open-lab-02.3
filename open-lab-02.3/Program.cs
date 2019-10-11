628a4292982b23cdd033af0c567a953c9622991f
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-11 22:14:04 +0200

    open-lab-02.3

---
+++ open-lab-02.3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace open_lab_02._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write two numbers.");
+            string number1 = Console.ReadLine();
+            string number2 = Console.ReadLine();
+            int a = Convert.ToInt32(number1);
+            int b = Convert.ToInt32(number2);
+            int c = a % b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
